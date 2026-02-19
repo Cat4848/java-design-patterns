6b8b67c0f03dea179d48292112f81b51ec66aa28
Cat4848 <catalinrol@gmail.com>
2026-02-19 08:36:03 +0000

    feat: added the DecoyDuck to the selection

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -17,5 +17,17 @@ public class Main {
     mallardDuck.setFlyBehavior(flyNoWay);
     System.out.println("Now that we changed its fly behavior, let's see it fly again...");
     mallardDuck.performFly();
+
+    System.out.println("-------------------------------");
+    System.out.println("Now it's time to switch gears!!");
+    System.out.println("Let's admire our newest acquisition...");
+
+    QuackBehavior squeak = new SqueakyQuack();
+    Duck decoyDuck = new DecoyDuck("Mike", flyNoWay, squeak);
+    System.out.println(decoyDuck.display());
+    System.out.println("Now let's see this one fly...");
+    decoyDuck.performFly();
+    System.out.println("It can squeak too.");
+    decoyDuck.performQuack();
   }
 }
